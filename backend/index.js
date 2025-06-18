66efceaec825627f88b3aa2ea6a988397a85b684
laksh-wadhwani <laksh.wadhwani55@gmail.com>
2025-06-19 02:01:28 +0500

    Updated backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ const dotenv = require('dotenv')
 
 const app = express();
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true, 
+}))
 dotenv.config()
 
 mongoose.connect(process.env.DB_URL)
